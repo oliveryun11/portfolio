a537913117db8f20d2b2e2b6bf4b0146d65e0bc2
Oliver Yun <otyun@Olivers-MacBook-Air.local>
2025-07-08 00:22:28 -0700

    meep

--- src/content/blog/kb-qa.tsx
+++ src/content/blog/kb-qa.tsx
@@ -102,11 +102,10 @@ const post: BlogPost = {
         questions and tested on multi-step questions. Our NSCL model achieves
         91.23% accuracy, outperforming a fine-tuned T5 baseline which achieves
         66.67% accuracy. Our findings demonstrate that this neuro-symbolic
-        architecture achieves superior compositional generalization, confirming
-        that an explicit reasoning framework is more robust for multi-step logic
-        than standard end-to-end approaches. We conclude by discussing future
-        directions for building more scalable and generalist neuro-symbolic
-        models by moving beyond predefined domain knowledge.
+        architecture achieves superior compositional generalization. We conclude
+        by discussing future directions for building more scalable and
+        generalist neuro-symbolic models by moving beyond predefined domain
+        knowledge.
       </p>
 
       <h2>Overview</h2>
